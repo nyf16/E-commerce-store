ed6a57d6b17f519cb4709c521b501f12568fccd4
Onur FERIK <49190459+nyf16@users.noreply.github.com>
2020-08-13 17:22:50 +0300

    Database

--- ECommerceStore/ECommerceStore.Application/Products/CreateProduct.cs
+++ ECommerceStore/ECommerceStore.Application/Products/CreateProduct.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ECommerceStore.Database;
+using ECommerceStore.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,21 @@ namespace ECommerceStore.Application.Products
 {
     public class CreateProduct
     {
-        
+        private ApplicationDbContext _context;
+
+        public CreateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Do(int id, string Name, string Description)
+        {
+            _context.Products.Add(new Product
+            {
+                Id = id,
+                Name = Name,
+                Description = Description
+            });
+        }
     }
 }
---
+++ ECommerceStore/ECommerceStore.Database/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ECommerceStore.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceStore.Database
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products{ get; set; }
+
+
+
+    }
+}
