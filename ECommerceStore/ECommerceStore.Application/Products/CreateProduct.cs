77f8e31c045885b2a8e6a63a6a8a9787f87adc44
Onur FERIK <49190459+nyf16@users.noreply.github.com>
2020-08-31 22:55:49 +0300

    Adding Products

--- ECommerceStore/ECommerceStore.Application/Products/CreateProduct.cs
+++ ECommerceStore/ECommerceStore.Application/Products/CreateProduct.cs
@@ -2,6 +2,7 @@
 using ECommerceStore.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,14 +18,16 @@ namespace ECommerceStore.Application.Products
             _context = context;
         }
 
-        public void Do(int id, string Name, string Description)
+        public async Task Do(string Name, string Description, decimal Value)
         {
             _context.Products.Add(new Product
             {
-                Id = id,
                 Name = Name,
-                Description = Description
+                Description = Description,
+                Value = Value
             });
+
+            await _context.SaveChangesAsync();
         }
     }
 }
--- ECommerceStore/ECommerceStore.UI/Pages/Index.cshtml.cs
+++ ECommerceStore/ECommerceStore.UI/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerceStore.Application.Products;
+using ECommerceStore.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -10,16 +12,38 @@ namespace ECommerceStore.UI.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
+        private ApplicationDbContext _ctx;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IndexModel(ApplicationDbContext ctx)
         {
-            _logger = logger;
+            _ctx = ctx;
+        }
+
+        [BindProperty]
+        public ProductViewModel Product { get; set; }
+
+        public class ProductViewModel
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
         }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await new CreateProduct(_ctx).Do(Product.Name, Product.Description, Product.Value);
+            return RedirectToPage("Index");
         }
     }
 }
