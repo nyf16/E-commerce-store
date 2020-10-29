6a7b1b01983fa4a530a33eef4f6a19f8f4f7115c
Onur FERIK <49190459+nyf16@users.noreply.github.com>
2020-10-29 19:06:44 +0300

    AddingVue

--- ECommerceStore/ECommerceStore.UI/Startup.cs
+++ ECommerceStore/ECommerceStore.UI/Startup.cs
@@ -27,7 +27,8 @@ namespace ECommerceStore.UI
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["DefaultConnection"]));
 
-            services.AddRazorPages();
+            services.AddRazorPages().
+                AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
